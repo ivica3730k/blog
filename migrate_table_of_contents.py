4b4dcf14d61d04fab4db7595ec630276ce5651b1
ivica3730k <ivica3730k@users.noreply.github.com>
2023-01-14 23:33:30 +0000

    deploy: 2a18e1b2019421b41f6600fe6b0631e4224acef5

---
+++ migrate_table_of_contents.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    toc_table = """
+# Table of Contents
+| Project | Description |
+| --- | --- |
+"""
+    for root, folder, file in os.walk("./projects/"):
+        if root == "./projects/":
+            continue
+        if file.count("README.md"):
+            with open(os.path.join(root, "README.md")) as f:
+                title = ""
+                description = ""
+                readme = f.read()
+                lines = readme.splitlines()
+                index_of_title = 0
+                for i in lines:
+                    if i.startswith("#"):
+                        title = i[2:]
+                        break
+                    index_of_title += 1
+                if not title:
+                    sys.exit("No title found")
+                print(f"title: {title}")
+                description = lines[index_of_title+1]
+                print(f"description: {description}")
+            toc_table += f"""| [{title}]({root}/README.md) | {description} |\n"""
+
+    main_readme = open("README.md").read()
+    main_readme_lines = main_readme.splitlines()
+    try:
+        start_index_of_toc = main_readme_lines.index("# Table of Contents")
+    except ValueError:
+        sys.exit("No table of contents found in README.md")
+    try:
+        end_index_of_toc = main_readme_lines.index(
+            "<!--Table of contents end, do not remove this comment -->")
+    except ValueError:
+        sys.exit("No end of table of contents found in README.md")
+
+    contents_before_toc_table = "\n".join(
+        main_readme_lines[:start_index_of_toc])
+    contents_after_toc_table = "\n".join(main_readme_lines[end_index_of_toc:])
+
+    new_readme = f"""{contents_before_toc_table}{toc_table}{contents_after_toc_table}"""
+
+    with open("README.md", "w") as f:
+        f.write(new_readme)
